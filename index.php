5ee365d9198f11ebc9b2ce078b3800880a519dd2
C6je in je kontgat <159583352+Pandoraxx1@users.noreply.github.com>
2025-02-27 09:47:03 +0100

    begin van hoofdpagina html en css

--- index.php
+++ index.php
@@ -1,18 +1,21 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="nl">
-
 <head>
-    <title>Taakbord</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Taakbord - DeveloperLand</title>
+    <link rel="stylesheet" href="css/main.css">
     <?php require_once 'head.php'; ?>
 </head>
-
 <body>
-    
-    <div class="container">
-
-
-    </div>
-
+    <header>
+        <div class="header-content">
+            <img src="img/DeveloperLandLogo.png" alt="DeveloperLand logo" class="logo">
+            <h1>Taakbord</h1>
+        </div>
+    </header>
+    <main>
+        <p>Welkom bij DeveloperLand! Dit is het taakbord waar je al je taken kunt beheren.</p>
+    </main>
 </body>
-
 </html>
