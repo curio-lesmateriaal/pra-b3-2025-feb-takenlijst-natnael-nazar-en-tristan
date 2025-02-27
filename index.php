3c6b04058fc6cecf773ef465e9715590b793e035
C6je in je kontgat <159583352+Pandoraxx1@users.noreply.github.com>
2025-02-27 10:12:01 +0100

    css

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Taakbord</title>
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/main.css">
     
     
     <?php require_once 'head.php'; ?>
