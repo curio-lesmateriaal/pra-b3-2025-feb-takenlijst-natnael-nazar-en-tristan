d920942a15c15656a532cdff46e7894664b4edb8
emxnator <nazar251107@gmail.com>
2025-02-27 10:29:01 +0100

    Update login.php

---
+++ login.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Taakbord</title>
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/main.css">
+    
+    
+    <?php require_once 'head.php'; ?>
+</head>
