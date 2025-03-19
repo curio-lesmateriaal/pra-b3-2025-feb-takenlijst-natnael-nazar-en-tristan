67386e8815d0beb556e72a9688e77b8522ea0825
C6je in je kontgat <159583352+Pandoraxx1@users.noreply.github.com>
2025-03-19 11:57:49 +0100

    comments weg

--- app/http/Controllers/LoginController.php
+++ app/http/Controllers/LoginController.php
@@ -6,17 +6,16 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = $_POST['user'];
     $password = $_POST['pass'];
 
-    // Gebruik de juiste tabel- en kolomnamen
     $query = "SELECT * FROM `users` WHERE username = :username";
     $stmt = $conn->prepare($query);
     $stmt->execute([':username' => $username]);
     $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
     if ($user && $password === $user['password']) {
-        // Inloggen succesvol
+ 
         $_SESSION['user'] = $username; 
         header('Location: /task/index.php');
- // Doorsturen naar takenlijst
+
         exit;
     } else {
         // Inloggen mislukt
